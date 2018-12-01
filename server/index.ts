e8cd21453a70fabbed0b2e6d0fdcbadedc45f4ee
andrerpena <andrerpena@gmail.com>
2018-12-01 20:58:37 +0100

    Add Express

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,26 @@
-import { createServer } from 'http'
-import { parse } from 'url'
 import * as next from 'next'
+import * as express from 'express'
+import { fromExpressRequest } from './util/url'
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const nextServer = next({ dev })
+const nextRequestHandler = nextServer.getRequestHandler()
 
-app.prepare()
+nextServer.prepare()
   .then(() => {
-    createServer((req, res) => {
-      const parsedUrl = parse(req.url || '', true)
-      const { pathname, query } = parsedUrl
+    const app = express()
 
-      if (pathname === '/a') {
-        app.render(req, res, '/about', query)
-      } else if (pathname === '/b') {
-        app.render(req, res, '/b', query)
-      } else {
-        handle(req, res, parsedUrl)
-      }
+    app.get('/a', (req, res) => {
+      nextServer.render(req, res, '/about', req.query)
     })
-      .listen(port, (err: Error) => {
-        if (err) throw err
-        console.log(`> Ready on http://localhost:${port}`)
-      })
+
+    app.get('*', (req, res) => {
+      nextRequestHandler(req, res, fromExpressRequest(req))
+    })
+
+    app.listen(port, (err: Error) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
   })
---
+++ server/util/url.ts
@@ -0,0 +1,14 @@
+import { Request } from 'express'
+
+type UrlWithQuery = URL & { query: Record<string, any> }
+
+export function fromExpressRequest(request: Request) {
+  // TODO: Make this to look better and add some tests
+  const url: Partial<UrlWithQuery> = {
+    protocol: request.protocol,
+    host: request.hostname,
+    pathname: request.path,
+    query: request.query,
+  }
+  return url
+}
