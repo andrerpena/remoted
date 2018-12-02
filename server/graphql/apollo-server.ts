1f3f36d85e0b92268d900f8aac5df6049bae15a9
andrerpena <andrerpena@gmail.com>
2018-12-02 16:09:05 +0100

    Add type generation for resolvers

--- server/graphql/apollo-server.ts
+++ server/graphql/apollo-server.ts
@@ -1,12 +1,29 @@
 import { importSchema } from "graphql-import";
 import { ApolloServer, gql } from "apollo-server-express";
+import { Resolvers } from "./resolver-types";
 
 const typeDefs = gql(importSchema("server/graphql/schema.graphql"));
 
-const resolvers = {
+type ResolversType = Resolvers & {
+  [key: string]: any;
+};
+
+const resolvers: ResolversType = {
   Query: {
     hello() {
       return "world";
+    }
+  },
+  Company: {
+    name: () => {
+      return "this is a company name";
+    }
+  },
+  Mutation: {
+    addCompany: () => {
+      return {
+        name: "something"
+      };
     }
   }
 };
