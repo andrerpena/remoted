4cad07c3a986af446c31dfd2013ff146d67d349c
andre <andrerpena@gmail.com>
2019-04-06 17:39:16 +0200

    Serch visuals are almost done

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -16,7 +16,7 @@ export const Checkbox: React.FunctionComponent<CheckboxProps> = (
         type="checkbox"
         id={props.id}
         checked={props.checked}
-        onChange={v => props.onChange(!props.checked)}
+        onChange={() => props.onChange(!props.checked)}
       />
       <label htmlFor={props.id}>{props.label}</label>
     </div>
