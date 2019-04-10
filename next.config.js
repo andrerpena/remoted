cb4b0aa48e043d3fca5d6280b83ba44c256fbb48
andre <andrerpena@gmail.com>
2019-04-10 08:20:34 +0200

    Possibility to generate bundle reports

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
 require("dotenv").config();
 const withTypescript = require("@zeit/next-typescript");
 const withSass = require("@zeit/next-sass");
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 
 module.exports = withTypescript(
-  withSass({
-    publicRuntimeConfig: {
-      graphqlEndPoint: process.env.GRAPHQL_URI
-    }
-  })
+  withSass(
+    withBundleAnalyzer({
+      analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: "static",
+          reportFilename: "../bundles/server.html"
+        },
+        browser: {
+          analyzerMode: "static",
+          reportFilename: "../bundles/client.html"
+        }
+      },
+      publicRuntimeConfig: {
+        graphqlEndPoint: process.env.GRAPHQL_URI
+      }
+    })
+  )
 );
