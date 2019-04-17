50cc83d4de35c025a8aa34bc703b90d722c793a5
andre <andrerpena@gmail.com>
2019-04-17 06:48:22 +0200

    Make the menu to work on mobile

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,45 +1,60 @@
 import "./NavBar.scss";
 import { Logo } from "./Logo";
 import * as React from "react";
+import * as classNames from "classnames";
 
-export const NavBar = () => (
-  <nav
-    className="main-nav navbar"
-    role="navigation"
-    aria-label="main navigation"
-  >
-    <div className="container">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="/">
-          <Logo />
-        </a>
-        <a
-          role="button"
-          className="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start" />
-        <div className="navbar-end">
-          <a
-            className="navbar-item"
-            href="https://github.com/remoted-io/remoted/issues"
-          >
-            🔥 Issues
+export const NavBar = () => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <nav
+      className="main-nav navbar"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            <Logo />
           </a>
-          <a className="navbar-item" href="https://remoted.io/graphql">
-            🤖 GraphQL API
+          <a
+            role="button"
+            className={classNames("navbar-burger", "burger", {
+              "is-active": open
+            })}
+            aria-label="menu"
+            aria-expanded={open ? "true" : "false"}
+            onClick={() => setOpen(!open)}
+            data-target="main-menu"
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
           </a>
         </div>
+
+        <div
+          id="main-menu"
+          className={classNames("navbar-menu", { "is-active": open })}
+        >
+          <div className="navbar-start" />
+          <div className="navbar-end">
+            <a
+              className="navbar-item"
+              target="__blank"
+              href="https://github.com/remoted-io/remoted/issues"
+            >
+              🔥 Issues
+            </a>
+            <a
+              className="navbar-item"
+              target="__blank"
+              href="https://remoted.io/graphql"
+            >
+              🤖 GraphQL API
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
