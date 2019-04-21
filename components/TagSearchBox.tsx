1a9a7457a079533207954fffbebae60f30025d34
andre <andrerpena@gmail.com>
2019-04-21 17:41:37 +0200

    make it clearer that I'm searching by tag

--- components/TagSearchBox.tsx
+++ components/TagSearchBox.tsx
@@ -100,7 +100,7 @@ export class TagSearchBox extends React.Component<
     const { value, suggestions } = this.state;
 
     const inputProps: InputProps<TagOption> = {
-      placeholder: "Search remote jobs",
+      placeholder: "Search remote jobs by tag",
       value,
       onChange: this.onChange,
       onBlur: this.onBlur,
