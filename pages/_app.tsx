002dd0defc223def09eda0d9bb5895ec2d74df3e
apena <andre.pena@zalando.de>
2019-04-25 22:43:42 +0200

    Adding default SEO configuration and log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,36 @@ import React from "react";
 import withApolloClient from "../lib/common/with-apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-boost";
+import NextSeo from "next-seo";
+
+export const SEO = {
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://remoted.io",
+    site_name: "Remoted.io",
+    images: [
+      {
+        url: "https://remoted.io/static/logo.png",
+        width: 160,
+        height: 160,
+        alt: "Remoted.io logo"
+      }
+    ]
+  },
+  twitter: {
+    handle: "@andrerpena",
+    site: "@andrerpena",
+    cardType: "summary_large_image"
+  }
+};
 
 class MyApp extends App<{ apolloClient: ApolloClient<any> }> {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
       <Container>
+        <NextSeo config={SEO} />
         <ApolloProvider client={apolloClient}>
           <Component {...pageProps} />
         </ApolloProvider>
