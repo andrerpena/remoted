8de08dea247682eb99b6ab877969fa7fef38b584
andre <andrerpena@gmail.com>
2019-05-26 14:52:48 +0200

    Fix playground

--- lib/common/object.ts
+++ lib/common/object.ts
@@ -1,5 +1,8 @@
 export function merge(target: any, ...sources: any[]) {
   for (const source of sources) {
+    if (!source) {
+      continue;
+    }
     for (const key of Object.keys(source)) {
       const val = source[key];
       if (val !== undefined && val !== null) {
