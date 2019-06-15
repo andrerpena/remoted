1429993cad6de7f033f5395c553e76049e2af75f
andre <andrerpena@gmail.com>
2019-06-15 21:09:38 +0200

    Add e-mail templates and do some experiments setting pug e-mail templates

--- playground.ts
+++ playground.ts
@@ -1,10 +1,44 @@
-// import { config } from "dotenv";
-//
-// config();
-// sendEmail()
-//   .then(() => {
-//     console.log("awesome");
-//   })
-//   .catch(ex => {
-//     console.log(ex);
-//   });
+import { config } from "dotenv";
+import { buildDb } from "./server/db/build-db";
+import { searchJobs } from "./server/graphql/services/job-service";
+import * as path from "path";
+import * as Email from "email-templates";
+
+config();
+
+buildDb().then(async db => {
+  const jobs = await searchJobs(db, 10, 0);
+  console.log(jobs);
+
+  const email = new Email({
+    message: {
+      from: "niftylettuce@gmail.com"
+    },
+    // uncomment below to send emails in development/test env:
+    // send: true
+    transport: {
+      jsonTransport: true
+    },
+    juice: true,
+    juiceResources: {
+      preserveImportant: true,
+      webResources: {
+        relativeTo: path.resolve("emails/newsletter"),
+        images: true
+      }
+    }
+  });
+
+  const emailResult = await email.send({
+    template: "newsletter",
+    message: {
+      to: "elon@spacex.com"
+    },
+    locals: {
+      name: "Elon",
+      jobs: [1, 2, 3, 4, 5]
+    }
+  });
+
+  console.log(emailResult);
+});
